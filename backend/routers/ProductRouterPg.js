b9f109edacc6c04477ca5294d076b8622aac223b
Gonzalez Pacheco Daniel <GonzalezPachecoDaniel@JohnDeere.com>
2022-09-21 10:20:03 -0500

    router pg changes

--- backend/routers/ProductRouterPg.js
+++ backend/routers/ProductRouterPg.js
@@ -1,32 +1,37 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import * as pg from 'pg';
-
-const { Pool } = pg.default;
-
-const clientPg = new Pool({
-  user: 'postgres',
-  host: 'amazona.c4vurfvhjghf.us-east-1.rds.amazonaws.com',
-  database: 'amazona',
-  password: 'masterkey',
-  port: 5432,
-});
+import { Client } from 'ts-postgres';
 
 const productRouterpg = express.Router();
 
 productRouterpg.get(
   '/',
   expressAsyncHandler(async (req, res) => {
-    await clientPg.query('SELECT * FROM PRODUCTS', (err, respg) => {
-      if (err) {
-        console.error(err);
-        res.status(404).send({ message: 'connection error: ' + err.message });
-      } else {
-        console.log(respg);
-        respg.end();
-        res.send(respg);
-      }
+    const client = new Client({
+      user: 'postgres',
+      host: 'amazona.c4vurfvhjghf.us-east-1.rds.amazonaws.com',
+      database: 'amazona',
+      password: 'masterkey',
+      port: 5432,
     });
+
+    await client.connect();
+
+    try {
+      // Querying the client returns a query result promise
+      // which is also an asynchronous result iterator.
+      const result = await client.execute('select * from products');
+      console.log(result);
+      console.log('after print result');
+
+      for await (const row of result) {
+        // 'Hello world!'
+        console.log(row.get('message'));
+      }
+      res.send(result);
+    } finally {
+      await client.end();
+    }
   })
 );
 
